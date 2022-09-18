51d5cb256e950043aca43b7649c47d30cb056976
IvanKudinov <root@DESKTOP-8LMVP1L.localdomain>
2022-09-18 17:14:28 +0300

    Изменение конфига в docker-compose es и добавление комментариев в entrypoint.sh etl-проекта

--- postgres_to_es/entrypoint.sh
+++ postgres_to_es/entrypoint.sh
@@ -9,9 +9,13 @@ while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
       sleep 0.1
 done
 
+echo "Postgres is ready"
+
 while ! nc -z $ELASTIC_HOST $ELASTIC_PORT; do
       echo "Waiting for es"
       sleep 0.1
 done
+
+echo "ES is ready"
 
 python main.py
